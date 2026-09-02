e75ee6bf3611362e4180ec1546d9f8d6fb698a70
mishra-anik <anikmishra352@gmail.com>
2026-09-02 12:06:35 +0530

    remove dist folder

--- server/dist/src/router/chat.route.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import chatController from "../controllers/chat.controller.js";
-const chatRoute = Router();
-chatRoute.post("/chat", chatController);
-export default chatRoute;
